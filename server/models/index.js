4f3a76b3a03887bf2fb033953a3db2f3e3947d20
Sojisoyoye <soji.soyoye@gmail.com>
2019-02-03 16:50:36 +0100

    ch(database): setup postgres database - persisted data into database - write sql queries to write and read from the endpoints [Delivers #163673865]

---
+++ server/models/index.js
@@ -0,0 +1,27 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+export default {
+  /**
+     * @method Database query
+     * @param {object} request object
+     * @param {object} response object
+     * @returns {object} object
+     */
+
+  query(text, params) {
+    return new Promise((resolve, reject) => {
+      pool.query(text, params)
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,53 +1,32 @@
+import 'babel-polyfill';
 import express from 'express';
+import dotenv from 'dotenv';
 import PartyController from '../controllers/partycontroller';
 import OfficeController from '../controllers/officecontroller';
-import validation from '../middleware/validator';
+//  import validation from '../middleware/validator';
 
+dotenv.config();
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to Politico API version1' });
 });
 
-router.post(
-  '/parties',
-  validation.validateParty,
-  validation.validationHandler,
-  PartyController.postParty,
-);
+router.post('/parties', PartyController.postParty);
 
 router.get('/parties', PartyController.getParties);
 
-router.get(
-  '/parties/:id',
-  validation.validatePartyId,
-  validation.validationHandler,
-  PartyController.getAParty,
-);
+router.get('/parties/:id', PartyController.getAParty);
 
-router.patch(
-  '/parties/:id/name',
-  validation.validatePartyId,
-  validation.validationHandler,
-  PartyController.updateName,
-);
+router.patch('/parties/:id/name', PartyController.updateName);
 
 router.delete('/parties/:id', PartyController.deleteParty);
 
-router.post(
-  '/offices',
-  validation.validateOffice,
-  validation.validationHandler,
-  OfficeController.postOffice,
-);
+router.post('/offices', OfficeController.postOffice);
 
 router.get('/offices', OfficeController.getOffices);
 
-router.get(
-  '/offices/:id',
-  validation.validateOfficeId,
-  validation.validationHandler,
-  OfficeController.getAOffice,
-);
+router.get('/offices/:id', OfficeController.getAOffice);
+
 
 export default router;
